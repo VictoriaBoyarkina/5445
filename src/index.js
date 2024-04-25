1fb16dd001fc4ecd3b55347deb889e6721e7260d
Avdeev Victor <vavdeev@zuzex.com>
2024-04-25 11:21:40 +0300

    feat: add routes and socket for common task after education

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-import cookieParser from "cookie-parser";
-import express from "express";
-import { createServer } from "http";
-import cors from "cors";
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import { createServer } from 'http';
+import cors from 'cors';
 
-import { initGraphQL } from "./serviceCommon/graphql/index.js";
-import { httpRouter } from "./serviceCommon/http/index.js";
-import { cookieRouter } from "./serviceCommon/cookie/index.js";
-import { xhrRouter } from "./serviceCommon/xhr/index.js";
-import { axiosRouter } from "./serviceCommon/axios/index.js";
-import { notificationsRouter } from "./browserRare/notifications/index.js";
-import { rxjsRouter } from "./serviceRare/RxJS/index.js";
-import { reactRoutingGuardsRouter } from "./reactCommon/routingGuard/index.js";
+import { initGraphQL } from './serviceCommon/graphql/index.js';
+import { httpRouter } from './serviceCommon/http/index.js';
+import { cookieRouter } from './serviceCommon/cookie/index.js';
+import { xhrRouter } from './serviceCommon/xhr/index.js';
+import { axiosRouter } from './serviceCommon/axios/index.js';
+import { notificationsRouter } from './browserRare/notifications/index.js';
+import { rxjsRouter } from './serviceRare/RxJS/index.js';
+import { reactRoutingGuardsRouter } from './reactCommon/routingGuard/index.js';
+import { initSocket } from './mentorTask/socket.js';
+import { chartDataRouter } from './mentorTask/chartData.js';
+import { authRouter } from './mentorTask/auth.js';
+import { templateDataRouter } from './mentorTask/template.js';
 
 const PORT = process.env.PORT || 4000;
-const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000";
+const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000';
 
 const app = express();
 const httpServer = createServer(app);
@@ -25,45 +29,52 @@ app.use(
   express.urlencoded({
     // to support URL-encoded bodies
     extended: true,
-  })
+  }),
 );
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", [CLIENT_URL]);
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.setHeader('Access-Control-Allow-Origin', [CLIENT_URL]);
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   next();
 });
 
 await initGraphQL(app, httpServer);
 
+/* Mentor Task START */
+initSocket(app, httpServer);
+app.use('/commonTask/chartData', chartDataRouter);
+app.use('/commonTask/auth', authRouter);
+app.use('/commonTask/template', templateDataRouter);
+/* Mentor Task END */
+
 /* Browser Rare START */
-app.use("/notifications", notificationsRouter);
+app.use('/notifications', notificationsRouter);
 /* Browser Rare END */
 
 /* Service Common START */
-app.use("/http", httpRouter);
-app.use("/cookie", cookieRouter);
-app.use("/xhr", xhrRouter);
-app.use("/axios", axiosRouter);
+app.use('/http', httpRouter);
+app.use('/cookie', cookieRouter);
+app.use('/xhr', xhrRouter);
+app.use('/axios', axiosRouter);
 /* Service Common END */
 
 /* Service Rare START */
-app.use("/rxjs", rxjsRouter);
+app.use('/rxjs', rxjsRouter);
 /* Service Rare END */
 
 /* React Common START */
-app.use("/reactGuard", reactRoutingGuardsRouter);
+app.use('/reactGuard', reactRoutingGuardsRouter);
 /* React Common END */
 
 httpServer.listen(PORT, () => {
   console.log(
-    `✅ \x1b[35mServer is running on \x1b[36mhttp://localhost:${PORT}\x1b[0m`
+    `✅ \x1b[35mServer is running on \x1b[36mhttp://localhost:${PORT}\x1b[0m`,
   );
   console.log(
-    `✅ \x1b[35mGraphQL Query endpoint ready at \x1b[36mhttp://localhost:${PORT}/graphql\x1b[0m`
+    `✅ \x1b[35mGraphQL Query endpoint ready at \x1b[36mhttp://localhost:${PORT}/graphql\x1b[0m`,
   );
   console.log(
-    `✅ \x1b[35mGraphQl Subscription endpoint ready at \x1b[36mws://localhost:${PORT}/subscription\x1b[0m`
+    `✅ \x1b[35mGraphQl Subscription endpoint ready at \x1b[36mws://localhost:${PORT}/subscription\x1b[0m`,
   );
 });
---
+++ src/mentorTask/auth.js
@@ -0,0 +1,58 @@
+import express from 'express';
+
+const router = express.Router();
+
+const defaultUser = {
+  email: 'testuser@test.test',
+  password: 'test-password',
+};
+
+const tokenKey = 'token';
+const validToken =
+  'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJudm9yb2J5b3YiLCJleHAiOjE3MTI4MTczNjEsInJvbGUiOiJVU0VSIn0.8dQRJtlVkHqhyRLldnnHW_MQK6NSd--VJoAJjpWSMnGsOCV96Gk7SHwhPqiXO5IoR2YSFJz3T2za8sqjPTEusA';
+
+router.post('/login', (req, res) => {
+  const { email, password } = req.body ?? {};
+
+  if (email !== defaultUser.email || password !== defaultUser.password) {
+    return res.status(400).json({
+      status: 'error',
+      message: '`email` or `password` are not valid',
+    });
+  }
+
+  return res
+    .cookie(tokenKey, validToken, {
+      httpOnly: true,
+      path: '/commonTask',
+    })
+    .json({
+      status: 'ok',
+    });
+});
+
+/** @type {import("express").RequestHandler} */
+export function commonTaskTokenCheck(req, res, next) {
+  const token = req.cookies.token;
+
+  if (token !== validToken) {
+    return res.status(401).json({
+      error: true,
+      message: 'Unauthorized',
+    });
+  }
+
+  next();
+}
+
+router.get('/user', commonTaskTokenCheck, (req, res) => {
+  return res.json({ email: defaultUser.email });
+});
+
+router.delete('/logout', (req, res) => {
+  res.clearCookie(tokenKey).json({
+    status: 'ok',
+  });
+});
+
+export { router as authRouter };
