194012f91bed99eafc3aa0183bd6ce852e9a8582
vboyarkina <vboyarkina@zuzex.com>
2024-07-03 12:28:56 +0300

    test

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,11 @@
 import serverless from 'serverless-http';
 import app, { router } from '../src';
 
+router.get('/test', (req, res) => {
+  res.json({
+    tgest: ' aad',
+  });
+});
 app.use('/.netlify/functions/api', router);
 
 export const handler = serverless(app);
