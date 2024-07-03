dfe1f34e1e41a8449b5b76d27025bbff2f534cb0
vboyarkina <vboyarkina@zuzex.com>
2024-07-03 11:52:20 +0300

    test

--- .netlify/functions/types.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace NodeJS {
-  interface ProcessEnv {
-    readonly NODE_ENV: "development" | "production" | "test";
-    readonly PORT: string;
-    readonly CLIENT_URL: string;
-    readonly PUBLIC_VAPID_KEY: string;
-    readonly PRIVATE_VAPID_KEY: string;
-  }
-}
